f813d7893e728b69ca61a817fb5da7571fa2268a
Sailor <la237887@student.helha.be>
2025-09-23 15:27:20 +0200

    commit fichiers manquants

---
+++ models/Authentificator.py
@@ -0,0 +1,15 @@
+import bcrypt
+class Authentificator:
+    def __init__(self, userPwd, pwdToCheck):
+        self.userPwd = userPwd
+        self.pwdToCheck = pwdToCheck
+
+
+    def hash_password(self, password: str) -> str:
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=12))
+        return hashed.decode('utf-8')
+
+    #fonction pour vérifier le mot de passe
+    def authenticate(self) -> bool:
+        return bcrypt.checkpw(self.pwdToCheck.encode('utf-8'), self.userPwd.encode('utf-8'))
+
